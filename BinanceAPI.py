5b5322ee571efb62e865e1ec07012b43916d6577
Ozgen <ozgn45@gmail.com>
2018-02-05 00:14:40 +0300

    trial version is finished.

--- BinanceAPI.py
+++ BinanceAPI.py
@@ -532,6 +532,3 @@ class BinanceBot:
             print(time.time())
 
 
-bot = BinanceBot(short_symbol="VIBE", btcOrBnb="BTC", profitPercentage=0.1, walletPercentage=100, stop_loss_pecentage=6)
-# print(bot.client.ping())
-print(bot.run())
--- BinanceBotRunner.py
+++ BinanceBotRunner.py
@@ -1,5 +1,5 @@
 from BinanceAPI import BinanceBot
 
-bot = BinanceBot(short_symbol="VIBE", btcOrBnb="BTC", profitPercentage=0.1, walletPercentage=100, stop_loss_pecentage=6)
+bot = BinanceBot(short_symbol="VIBE", btcOrBnb="BTC", profitPercentage=1, walletPercentage=100, stop_loss_pecentage=6)
 # print(bot.client.ping())
 bot.run()
