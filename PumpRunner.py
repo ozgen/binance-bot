2e306bfa65848faaf01ffa96c758620d9c76f79c
Ozgen <ozgn45@gmail.com>
2018-03-08 03:14:20 +0300

    update exponential to number for buying order and pump bot

--- PumpRunner.py
+++ PumpRunner.py
@@ -3,7 +3,7 @@ import BinanceAPI
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--symbol', type=str, help='Market Symbol (Ex: XVGBTC - XVGETH)', required=True)
+    parser.add_argument('--symbol', type=str, help='Market Symbol (Ex: XVG - ETH)', required=True)
 
     coin_name = parser.parse_args()
     BinanceAPI.BinanceBot(str(coin_name.symbol).upper()).pumpBuyAndSell(short_symbol=str(coin_name.symbol).upper())
